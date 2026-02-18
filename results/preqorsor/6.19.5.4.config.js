b5e834aa57a18317e250e36bde3af4aa94eeb8fa
Silimate Robot <sv-tests@silimate.com>
2026-02-18 07:33:32 +0000

    Deploy refs/heads/master (build 22125506649) Build from cfa76cc828bff3cc1f953e2b2f7f48be11f13c6d

---
+++ results/preqorsor/6.19.5.4.config.js
@@ -0,0 +1,1 @@
+config_loader_data['preqorsor/6.19.5.4'] = [["","enum_prev",1,"logs/preqorsor/chapter-6/6.19.5.4--enum_prev.sv.log.html","tests/chapter-6/6.19.5.4--enum_prev.sv.html"]]
---
+++ results/preqorsor/7.9.3.config.js
@@ -0,0 +1,1 @@
+config_loader_data['preqorsor/7.9.3'] = [["","associative-arrays-exists",0,"logs/preqorsor/chapter-7/arrays/associative/methods/exists.sv.log.html","tests/chapter-7/arrays/associative/methods/exists.sv.html"]]
